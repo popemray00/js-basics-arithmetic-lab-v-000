c5b158972292d8b7b64f602138ce4e81b9146161
Michael Pope <popemray1986@gmail.com>
2018-06-02 22:56:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const newID = 1000000000 + oldID
 
 const ageIsValid = Number.isInteger()
 
-const randomNumber = Math.floor(Math.random() * 19) + 1
+const randomNumber = Math.floor(Math.random() * 10) + 1
 
 const randomInteger = Math.floor(Math.random() + Math.round(.49))
